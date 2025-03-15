c97be5c97db45a5ef9cd00e2a7ba3fd60fe71896
Micah Sanders <micahsan@seas.upenn.edu>
2025-03-14 23:40:58 -0400

    Update create_widgets() to use helper functions

--- gui.py
+++ gui.py
@@ -25,6 +25,7 @@ class MainApplication:
         self.import_dir = None
         self.export_dir = None
         self.selected_import_dir_lbl = None
+        self.canvas_widget = None
         self.plot_btn = None
         self.figure = None
         self.canvas = None
@@ -39,61 +40,39 @@ class MainApplication:
 
     def create_widgets(self):
         """Create and place all widgets"""
-        # Label: select import directory
-        sel_import_dir_lbl = tk.Label(
-            root, text="Select the directory containing scans:")
-        sel_import_dir_lbl.grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
+        sel_import_dir_lbl = self.create_label("Select Import Directory", 0, 0)
+        sel_import_dir_btn = self.create_button("Open", 0, 1, self.open_import_dir)
+        self.selected_import_dir_lbl = self.create_label("", 1, 0, 2)
 
-        # Button: browse import directories
-        sel_import_dir_btn = tk.Button(
-            root, text="Open", width=5, command=self.open_import_dir)
-        sel_import_dir_btn.grid(row=0, column=1, sticky=tk.W, padx=5, pady=5)
+        self.plot_btn = self.create_button("Plot", 1, 1, self.plot)
+        self.plot_btn.grid_remove()     # initially hidden
 
-        # Label: selected import directory
-        self.selected_import_dir_lbl = tk.Label(root, text='')
-        self.selected_import_dir_lbl.grid(row=1, column=0, columnspan=2,
-                                          sticky=tk.W,
-                                          padx=5, pady=5)
-
-        # Button: plot button
-        self.plot_btn = tk.Button(
-            self.root, text="Plot", width=5, command=self.plot)
-
-        # Canvas: blank canvas
         self.figure = Figure()
         self.canvas = FigureCanvasTkAgg(self.figure, self.root)
-        self.canvas.get_tk_widget().grid(row=2, column=0, columnspan=1, padx=5,
-                                         pady=5)
-        self.canvas.draw()
+        self.canvas_widget = self.canvas.get_tk_widget()
+        self.canvas_widget.grid(row=2, column=0, columnspan=1, padx=5, pady=5)
 
-        # Label: select export directory
-        self.sel_export_dir_lbl = tk.Label(root, text='')
+        self.sel_export_dir_lbl = self.create_label("Select directory to export data to:", 3, 0)
+        self.sel_export_dir_lbl.grid_remove()     # initially hidden
+        self.sel_export_dir_btn = self.create_button("Open", 3, 1, self.open_export_dir)
+        self.sel_export_dir_btn.grid_remove()
 
-        # Button: browse export directories
-        self.sel_export_dir_btn = tk.Button(
-            root, text="Open", width=5, command=self.open_export_dir)
+        self.selected_export_dir_lbl = self.create_label("", 4, 0, 2)
 
-        # Label: selected export directory
-        self.selected_export_dir_lbl = tk.Label(root, text='')
-        self.selected_export_dir_lbl.grid(row=5, column=0, columnspan=2,
-                                          sticky=tk.W,
-                                          padx=5, pady=5)
+        self.export_btn = self.create_button("Export", 4, 1, self.export)
+        self.export_btn.grid_remove()
 
-        # Button: export button
-        self.export_btn = tk.Button(self.root, text="Export", width=5,
-                                    command=self.export)
-
-        # Label: data exported successfully
-        self.exported_lbl = tk.Label(root, text='')
-        self.exported_lbl.grid(
-            row=6, column=0, columnspan=2, sticky=tk.W, padx=5, pady=5)
+        self.exported_lbl = self.create_label("Files exported successfully", 5, 0)
+        self.exported_lbl.grid_remove()
 
     def create_label(self, text, row, col, colspan=1):
+        # TODO: add boolean parameter for initially hidden (use grid_remove())
         label = tk.Label(self.root, text=text)
         label.grid(row=row, column=col, columnspan=colspan, sticky=tk.W, padx=5, pady=5)
         return label
 
     def create_button(self, text, row, col, command):
+        # TODO: add boolean parameter for initially hidden (use grid_remove())
         button = tk.Button(self.root, text=text, command=command)
         button.grid(row=row, column=col, sticky=tk.W, padx=5, pady=5)
         return button
@@ -106,12 +85,12 @@ class MainApplication:
         self.selected_import_dir_lbl.config(text='')
         self.figure.clear()
         self.canvas.draw()
-        self.sel_export_dir_lbl.config(text='')
-        self.sel_export_dir_btn.grid_forget()
-        self.plot_btn.grid_forget()
-        self.selected_export_dir_lbl.config(text='')
-        self.export_btn.grid_forget()
-        self.exported_lbl.config(text='')
+        self.sel_export_dir_lbl.grid_remove()
+        self.sel_export_dir_btn.grid_remove()
+        self.plot_btn.grid_remove()
+        self.selected_export_dir_lbl.grid_remove()
+        self.export_btn.grid_remove()
+        self.exported_lbl.grid_remove()
 
         # Check if the directory was selected
         if not self.import_dir:
@@ -124,7 +103,7 @@ class MainApplication:
                    for f in path.iterdir()):
             self.selected_import_dir_lbl.config(text="Selected directory doesn't "
                                                 "contain .ima or .dcm files")
-            self.plot_btn.grid_forget()
+            self.plot_btn.grid_remove()
             return
 
         # Display selected directory
@@ -132,7 +111,7 @@ class MainApplication:
             text=f"You selected: {self.import_dir}")
 
         # Present plot button
-        self.plot_btn.grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
+        self.plot_btn.grid()
 
     def plot(self):
         # Retrieve plot and associated data
@@ -145,39 +124,36 @@ class MainApplication:
         self.canvas.get_tk_widget().grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
         # Label: select export directory
-        self.sel_export_dir_lbl = tk.Label(
-            root, text="Select directory to export data to:")
-        self.sel_export_dir_lbl.grid(
-            row=3, column=0, sticky=tk.W, padx=5, pady=5)
+        self.sel_export_dir_lbl.grid()
 
         # Button: browse export directories
-        self.sel_export_dir_btn.grid(
-            row=3, column=1, sticky=tk.W, padx=5, pady=5)
+        self.sel_export_dir_btn.grid()
 
     def open_export_dir(self):
         # Open a directory selection dialog
         self.export_dir = fd.askdirectory(initialdir="/", title="Select file")
 
         # Clear selected directory label
-        self.selected_export_dir_lbl.config(text='')
-        self.exported_lbl.config(text='')
+        self.exported_lbl.grid_remove()
 
         # Check if the directory was selected
         if not self.export_dir:
             self.selected_export_dir_lbl.config(text="No directory selected")
-            self.export_btn.grid_forget()
+            self.selected_export_dir_lbl.grid()
+            self.export_btn.grid_remove()
             return
 
         # Display selected directory
         self.selected_export_dir_lbl.config(
             text=f"You selected: {self.export_dir}")
+        self.selected_export_dir_lbl.grid()
 
         # Present export button
-        self.export_btn.grid(row=5, column=1, sticky=tk.W, padx=5, pady=5)
+        self.export_btn.grid()
 
     def export(self):
         main_export(self.df, self.figure, self.title, self.export_dir)
-        self.exported_lbl.config(text='Files exported successfully')
+        self.exported_lbl.grid()
 
 
 # Main Execution Block
