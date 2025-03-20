f222ea1a4f411954ecc3621b8a02406a8b08c8e5
Micah Sanders <micahsan@seas.upenn.edu>
2025-03-20 15:14:37 -0400

    refactor to be object-oriented

--- image_analyzer.py
+++ image_analyzer.py
@@ -1,38 +1,33 @@
-import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
-def compute_pixel_counts(dicom_files):
-    """Computes counts based on pixel data and rescale function"""
-    sums = []
-
-    # Rescale function (Output units = mx+b, where x is stored value)
-    def rescale(file, x):
-        return file.RescaleSlope * x + file.RescaleIntercept
-
-    # Loop through the given files, rescale, and calculate sums
-    for ds in dicom_files:
-        pixel_array = rescale(ds, ds.pixel_array)
-        sums.append(int(pixel_array.sum()))
-
-    return sums
+X_LABEL = 'Slice Location (mm)'
+Y_LABEL = 'Counts (Bq/ml)'
 
 
-def enumerate_indices(files):
-    """Enumerates indices over the range of slice locations"""
-    return np.arange(files[0].SliceLocation,
-                     files[len(files) - 1].SliceLocation + 1,
-                     files[0].SliceThickness)
+class ImageAnalyzer:
+    def __init__(self, image_series, folder_name):
+        self.image_series = image_series
+        self.folder_name = folder_name
+        self.counts = []
 
+    def compute_counts(self):
+        """Computes counts based on pixel data and rescale function"""
+        # DICOM rescale function (Output units = mx+b, where x is stored value)
+        def rescale(ds):
+            return ds.RescaleSlope * ds.pixel_array + ds.RescaleIntercept
 
-def create_dataframe(*args):
-    """Creates Pandas DataFrame
+        self.counts = [rescale(ds).sum() for ds in self.image_series]
+        return self.counts
 
-    Args:
-        *args: A variable number of tuples, where each tuple is (name, value)
-    """
-    df = pd.DataFrame()
+    def generate_plot(self):
+        """Generates and returns a plot of the analysis"""
+        slice_locations = [int(f.SliceLocation) for f in self.image_series]
+        df = pd.DataFrame({X_LABEL: slice_locations, Y_LABEL: self.counts})
 
-    for tup in args:
-        df[tup[0]] = tup[1]
-
-    return df
+        plt.rcParams['figure.dpi'] = 800
+        df.plot(x=X_LABEL, y=Y_LABEL, legend=False)
+        plt.xlabel(X_LABEL)
+        plt.ylabel(Y_LABEL)
+        plt.title(self.folder_name)
+        return plt
