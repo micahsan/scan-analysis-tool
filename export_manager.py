24da77e3eff93f38acf186a2a0faefde0ed7233b
Micah Sanders <micahsan@seas.upenn.edu>
2025-03-20 16:31:04 -0400

    refactor to be object-oriented

--- export_manager.py
+++ export_manager.py
@@ -1,31 +1,13 @@
-# Utilities for plotting and exporting
-
-from matplotlib import pyplot as plt
 from pathlib import Path
 
 
-def plot_data(df, title, x_label, y_label):
-    """Plots the computed values"""
-    figure = plt.figure()
-    plot = figure.add_subplot()
-    plot.plot(df[x_label], df[y_label])
-    plot.set_title(title)
-    plot.set_xlabel(x_label)
-    plot.set_ylabel(y_label)
+class ExportManager:
+    def __init__(self, folder_path, file_name):
+        self.path = Path(folder_path)
+        self.file_name = file_name
 
-    return figure
+    def save_csv(self, df):
+        df.to_csv(f'{str(self.path)}/{self.file_name}_counts.csv', index=False)
 
-
-def export_data(df, figure, folder_path, title):
-    """Exports the data to a csv file at the specified location"""
-    path = Path(folder_path)
-
-    if not path.is_dir():
-        print('Output folder does not exist')
-        return
-
-    # Save .csv of data
-    df.to_csv(str(path) + '/' + title + '_counts.csv')
-
-    # Save .png of plot
-    figure.savefig(str(path) + '/' + title + '_counts.png')
+    def save_plot(self, plt):
+        plt.savefig(f'{str(self.path)}/{self.file_name}_counts.png')
