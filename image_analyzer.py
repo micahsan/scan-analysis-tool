839703f9d797f7f38193ca66ec04b221eb318aeb
Micah Sanders <micahsan@seas.upenn.edu>
2025-03-24 12:12:32 -0400

    refactor to rescale images in __init__

--- image_analyzer.py
+++ image_analyzer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -6,20 +7,22 @@ Y_LABEL = 'Counts (Bq/ml)'
 
 
 class ImageAnalyzer:
-    def __init__(self, image_series, folder_name):
-        self.image_series = image_series
+    def __init__(self, dataset_series, folder_name):
+        self.slice_locations = [int(f.SliceLocation) for f in dataset_series]
         self.folder_name = folder_name
         self.df = None
 
-    def compute_counts(self):
-        """Computes counts based on pixel data and rescale function"""
         # DICOM rescale function (Output units = mx+b, where x is stored value)
         def rescale(ds):
             return ds.RescaleSlope * ds.pixel_array + ds.RescaleIntercept
 
-        slice_locations = [int(f.SliceLocation) for f in self.image_series]
-        counts = [rescale(ds).sum() for ds in self.image_series]
-        self.df = pd.DataFrame({X_LABEL: slice_locations, Y_LABEL: counts})
+        self.images = np.array([rescale(ds) for ds in dataset_series])
+
+    def compute_counts(self):
+        """Computes counts based on pixel data and rescale function"""
+        counts = [i.sum() for i in self.images]
+        self.df = pd.DataFrame({X_LABEL: self.slice_locations, Y_LABEL:
+                                counts})
         return self.df
 
     def generate_plot(self):
